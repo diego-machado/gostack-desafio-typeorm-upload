3341b51d2ed035613fad6adc9d7793737816c39d
Diego Machado <diego.machadodarosa@gmail.com>
2020-08-08 00:59:30 -0300

    feat: add transaction endpoints

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactinRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactinRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exists');
+    }
+
+    await transactinRepository.remove(transaction);
   }
 }
 
